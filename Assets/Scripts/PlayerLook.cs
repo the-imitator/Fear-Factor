a29404b19cd93d6ac0a415d0e8760310342488f2
the-imitator <25367233+the-imitator@users.noreply.github.com>
2019-09-30 15:10:35 -0400

    Midterm Week 1 Commit :: Very basic test level; Player can move; FPS cam works; Physics holding so far

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] private float mouseXSensitivity;
+    [SerializeField] private float mouseYSensitivity;
+    private float verticalAngle = 0.0f;
+    private float clampAngle = 90.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CamRotate();
+    }
+
+    private void CamRotate() {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        transform.parent.Rotate(0f, mouseX * mouseXSensitivity, 0f);
+        verticalAngle -= mouseY * mouseYSensitivity;
+        verticalAngle = Mathf.Clamp(verticalAngle, -clampAngle, clampAngle);
+
+        transform.localEulerAngles = new Vector3(verticalAngle, transform.localEulerAngles.y, 0f);
+
+    }
+}
--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+    public ForceMode forceType;
+    bool isFalling;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        //jumping
+        if (Input.GetButton("Jump") && !isFalling) {
+            isFalling = true;
+            Jump(jumpForce, forceType);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal"); 
+        float vertical = Input.GetAxis("Vertical");
+        rb.MovePosition(transform.position + transform.TransformDirection(horizontal, 0, vertical) * Time.deltaTime * speed);
     }
+
+    private void Jump(float force, ForceMode type) {
+        rb.AddForce(transform.up * force, type);
+    }
+
+    private void OnCollisionStay(Collision collision) {
+        if (isFalling) {
+            isFalling = false;
+        }
+    }
+
+
 }
